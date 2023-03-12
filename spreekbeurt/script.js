8f3da8b50166e93a7b3a9b1a1bc51987328b5c0f
boombadaga <112242495+boombadaga@users.noreply.github.com>
2023-03-12 13:48:19 +0100

    update

--- spreekbeurt/script.js
+++
@@ -1,7 +0,0 @@
-function changeContent(html) {
-  document.getElementById("button").innerHTML = html;
-  setTimeout(revertContent, 3*1000)
-}
-function revertContent(revertText) {
-  document.getElementById("button").innerHTML = "<h1>Klik op mij!</h1>";
-}
